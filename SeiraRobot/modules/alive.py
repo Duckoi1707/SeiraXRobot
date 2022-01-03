b2726295f1ba4ff44a44c81176081bc88745916a
Duckoi1707 <91252600+Duckoi1707@users.noreply.github.com>
2022-01-03 14:45:40 +0700

    Update alive.py

--- SeiraRobot/modules/alive.py
+++ SeiraRobot/modules/alive.py
@@ -7,17 +7,17 @@ from pyrogram import __version__ as pyrover
 from SeiraRobot.events import register as MEMEK
 from SeiraRobot import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/51c4e8b6b40cd0cb66cea.jpg"
+PHOTO = "https://telegra.ph/file/ed27010abc3804bebbddd.jpg"
 
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
-  LUNA = "**Holla I'm Seira!** \n\n"
-  LUNA += "💎 **I'm Working Properly** \n\n"
-  LUNA += "💎 **My Master : [ꜱᴇᴛʜ★](https://t.me/xyzsethhh)** \n\n"
-  LUNA += f"💎 **Telethon Version : {tlhver}** \n\n"
-  LUNA += f"💎 **Pyrogram Version : {pyrover}** \n\n"
-  LUNA += "**Terima kasih sudah menambahkan Seira 💜**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/SeiraXRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/seirasupport")]]
+  LUNA = "**Xin Chào Tôi Là OGGY VN!** \n\n"
+  LUNA += "💎 **Tôi Đang Làm Việc Như Một Con Ong Thợ** \n\n"
+  LUNA += "💎 **Chủ Nhân Tôi Là : [OGGY Là Trai Zin :3](https://t.me/oggyvn)** \n\n"
+  LUNA += f"💎 **Phiên bản Telethon : {tlhver}** \n\n"
+  LUNA += f"💎 **Phiên bản Pyrogram : {pyrover}** \n\n"
+  LUNA += "**Cảm ơn vì đã thêm OGGY Vào Nhóm Ạ💜**"
+  BUTTON = [[Button.url("Hỗ Trợ Lệnh", "https://t.me/oggyvipbot?start=help"), Button.url("Hỗ Trợ", "https://t.me/oggyvn")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
 

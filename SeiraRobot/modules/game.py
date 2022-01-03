f1eb637910f4a8ca0e030fd1e785b27cbe054378
Duckoi1707 <91252600+Duckoi1707@users.noreply.github.com>
2022-01-04 02:06:18 +0700

    Add files via upload

---
+++ SeiraRobot/modules/game.py
@@ -0,0 +1,42 @@
+import os
+
+from SeiraRobot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.xogame(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    botusername = "@xobot"
+    noob = "play"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob)
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+
+@register(outgoing=True, pattern=r"^\.whisp(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    wwwspr = event.pattern_match.group(1)
+    botusername = "@whisperBot"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, wwwspr)
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+
+@register(outgoing=True, pattern=r"^\.mod(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    modr = event.pattern_match.group(1)
+    botusername = "@PremiumAppBot"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, modr)
+    await tap[0].click(event.chat_id)
+    await event.delete()

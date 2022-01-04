5bd5a186e803208a9151ada1469f045e5859bc71
Duckoi1707 <91252600+Duckoi1707@users.noreply.github.com>
2022-01-04 17:13:01 +0700

    Update random_api.py

--- SeiraRobot/modules/random_api.py
+++ SeiraRobot/modules/random_api.py
@@ -49,16 +49,16 @@ from pyrogram import filters
 from SeiraRobot import TEMP_DOWNLOAD_DIRECTORY, pbot
 
 
-@pbot.on_message(filters.command("boobs"))
+@pbot.on_message(filters.command("anh18+"))
 async def boobs(client, message):
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
     pic_loc = os.path.join(TEMP_DOWNLOAD_DIRECTORY, "bobs.jpg")
-    a = await message.reply_text("**Mencari Gambar Bugil**")
+    a = await message.reply_text("**Tìm kiếm hình ảnh khỏa thân**")
     await asyncio.sleep(0.5)
-    await a.edit("`Mengirim foto bugil...`")
+    await a.edit("`Gửi ảnh khỏa thân...`")
     nsfw = requests.get("http://api.oboobs.ru/noise/1").json()[0]["preview"]
     urllib.request.urlretrieve("http://media.oboobs.ru/{}".format(nsfw), pic_loc)
-    await client.send_photo(message.chat.id, pic_loc, caption="**Sange boleh, Goblok jangan**")
+    await client.send_photo(message.chat.id, pic_loc, caption="**Nó tuyệt vời, đừng ngu ngốc**")
     os.remove(pic_loc)
     await a.delete()
